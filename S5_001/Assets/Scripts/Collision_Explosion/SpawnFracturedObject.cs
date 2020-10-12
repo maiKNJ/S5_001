ed6b3ab15bad9e80f98ada5fd8490d2a66ea58f1
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2020-10-12 14:04:27 +0200

    explosion

---
+++ S5_001/Assets/Scripts/Collision_Explosion/SpawnFracturedObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFracturedObject : MonoBehaviour
+{
+    public GameObject originalObject;
+    public GameObject fracturedObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SpawnFracturedObj();
+        }
+    }
+
+    public void SpawnFracturedObj()
+    {
+        if (originalObject != null)
+        {
+        Destroy(originalObject);
+            GameObject fracObj = Instantiate(fracturedObject, originalObject.transform.position, Quaternion.identity) as GameObject;
+        fracObj.GetComponent<Explosion>().Explode();
+    }        }
+}

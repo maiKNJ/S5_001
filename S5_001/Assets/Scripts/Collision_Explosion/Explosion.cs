0926eb418318c14fabd41c5192788c03bf865077
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2020-10-12 14:27:22 +0200

    Revert "Revert "explosion""
    This reverts commit b5efe099aef5cae187d0833667b47ffa8b2beb22.

---
+++ S5_001/Assets/Scripts/Collision_Explosion/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject explosion;
+    public Vector3 explosionOffset;
+
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+
+    public void Explode()
+    {
+        if (explosion != null)
+        {
+            GameObject explosionFX = Instantiate(explosion, transform.position + explosionOffset, Quaternion.identity) as GameObject;
+            Destroy(explosionFX, 5);
+        }
+
+        foreach (Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+        }
+
+    }
+
+
+}

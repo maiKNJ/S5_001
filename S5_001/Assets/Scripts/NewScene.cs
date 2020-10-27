e3c660a156343ac47851c44097482522128ca321
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2020-10-27 10:05:59 +0100

    NewScene script

---
+++ S5_001/Assets/Scripts/NewScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewScene : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("earth", LoadSceneMode.Single);
+    }
+}

5e1101f6ab90226e7aaf9193663a3cee9a082b04
CatharinaP <43847782+CatharinaP@users.noreply.github.com>
2020-11-02 15:27:36 +0100

    EndScene_EarthBasedLaser

---
+++ S5_001/Assets/Scripts/LaserRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRotation : MonoBehaviour
+{
+    public float speed = 2f;
+    public float maxRotation = 45f;
+
+    void Update()
+    {
+        float rotationZ = maxRotation * Mathf.Sin(Time.time * speed);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+    }
+
+}
--- S5_001/Assets/Scripts/Laser_EndScene.cs
+++ S5_001/Assets/Scripts/Laser_EndScene.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Laser_EndScene : MonoBehaviour, IPooledObject
 {
     //public float dir = -1f;
     //public float range2 = 1f;
-    public float speed = 1;
+    public float speed = 10;
     public GameObject LaserBeamOrigin;
-   
+
     public void OnObjectSpawn()
     {
         //Quaternion dir = LaserBeamOrigin.transform.rotation;
@@ -19,9 +20,10 @@ public class Laser_EndScene : MonoBehaviour, IPooledObject
         Vector3 direction = new Vector3(-1+transform.eulerAngles.z, 0, 0).normalized;
         transform.position += direction * speed * Time.deltaTime;*/
         //transform.rotation = dir;
-
+        // Vector3 direction = Vector3.Scale(Vector3.one, LaserBeamOrigin.transform.position);
         Rigidbody instBulletRid = GetComponent<Rigidbody>();
-        instBulletRid.AddForce(Vector3.left * speed);
+        instBulletRid.AddForce(Vector3.up * speed);
+
         //transform.position += (speed * transform.forward).normalized;
         // transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
@@ -30,6 +32,14 @@ public class Laser_EndScene : MonoBehaviour, IPooledObject
         direction.Normalize();
         Vector3 newVelocity = speed * direction;
         transform.position += newVelocity * Time.deltaTime;*/
+
     }
 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "IgnoreLowOrbit")
+        {
+            Physics.IgnoreCollision(GetComponent<Collider>(), col.gameObject.GetComponent<Collider>(), true);
+        }
+    }
 }

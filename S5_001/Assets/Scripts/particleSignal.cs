e8c2cec8fcf8b4e0a2109045724e5b87dfeaaf3a
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2020-11-12 09:23:25 +0100

    cutscene3- signal

---
+++ S5_001/Assets/Scripts/particleSignal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleSignal : MonoBehaviour {
+
+
+    // In this example we have a Particle System emitting aligned particles; we then emit and override the rotation every 2 seconds.
+
+    private ParticleSystem system;
+
+    public int speed;
+
+    public Transform target;
+
+    void Start()
+    {
+       
+    }
+
+
+    private void Update()
+
+
+
+    {      var go = GameObject.Find("finalSignal");
+ 
+            var shooterTransform = GameObject.Find("signal").transform;
+        float step = speed * Time.deltaTime;
+
+        Vector3 target = Vector3.RotateTowards(transform.forward, shooterTransform.position, step, 0.0f);
+        go.transform.rotation = Quaternion.LookRotation(target);
+
+  
+    }
+
+ 
+}
--- S5_001/Assets/Scripts/timedScript.cs
+++ S5_001/Assets/Scripts/timedScript.cs
@@ -6,20 +6,38 @@ public class timedScript : MonoBehaviour
 {
 
     public GameObject spawnee;
-    public float spawnTime;
-    public float spawnDelay;
-   
+//    public float spawnTime;
+
+    //public float spawnDelay;
+  //  int timerNoise;
+
+    float nextTime;
+    float modifier;
+
 
     // Use this for initialization
     void Start()
 
     {
+        nextTime = 0;
+       
+    }
+
+    public void Update()
+    {
+
+
+        modifier = Random.Range(1, 10);
+
+
+        nextTime = Time.time + modifier;
+        if(Time.time > nextTime)
+        {
+            Instantiate(spawnee, transform.position, transform.rotation);
+        }
+       
 
         
-
-
-        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
-       
     }
 
     public void SpawnObject()

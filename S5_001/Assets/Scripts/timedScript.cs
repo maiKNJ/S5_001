7ac10cb010427ef710a02be9f7a6056be07a8e36
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2020-11-18 09:13:10 +0100

    cutscene 3

--- S5_001/Assets/Scripts/timedScript.cs
+++ S5_001/Assets/Scripts/timedScript.cs
@@ -4,61 +4,30 @@ using UnityEngine;
 
 public class timedScript : MonoBehaviour
 {
-
     public GameObject spawnee;
-//    public float spawnTime;
-
-    //public float spawnDelay;
-  //  int timerNoise;
-
     float nextTime;
     float modifier;
-
     public float timeRemaining = 5;
     public bool timerIsRunning = false;
 
 
     private void Start()
     {
-        // Starts the timer automatically
         timerIsRunning = true;
-
         modifier = Random.Range(0, 3);
-
     }
 
     public void Update()
     {
-        
-
-  
-
-
-
-        if (timerIsRunning)
-        {
-            if (timeRemaining > 0)
-            {
-          
+        if (timerIsRunning){
+            if (timeRemaining > 0) {
                 timeRemaining -= Time.deltaTime;
-
-
             }
-            else
-            {
-
+            else{
                 Instantiate(spawnee, transform.position, transform.rotation);
                 timeRemaining = 1 + modifier;
-            }
-        }
+            }}}
 
-
-    }
-
-    public void SpawnObject()
-    {
-
+    public void SpawnObject() {
         Instantiate(spawnee, transform.position, transform.rotation);
-      
-    }
-}
+       }}

aee6baebe6122a010f9087f1190dd4cad4605186
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2020-11-24 12:24:04 +0100

    updates

---
+++ S5_001/Assets/changeLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeLight : MonoBehaviour
+{ 
+    // Interpolate light color between two colors back and forth
+    float duration = 10.0f;
+    Color color0 = Color.green;
+    Color color1 = Color.red;
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // set light color
+     
+        lt.color = color1;
+    }
+}
